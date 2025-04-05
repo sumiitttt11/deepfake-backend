f1e304b69b6da6bd200e5d6943fcdcf84fa7414c
Sumit Kumawat <kumawatsumit984@gmail.com>
2025-04-05 16:58:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,3 +13,8 @@ app.add_middleware(
 )
 
 app.include_router(predict.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Use Render's port or 8000 locally
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
